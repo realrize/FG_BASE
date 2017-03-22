cf1dbe09a7ec64f82128fc9aea996d086b0c80bd
realrize <rockmaxi@gmail.com>
2017-03-22 18:01:44 +0900

    Signed-off-by: realrize <rockmaxi@gmail.com>

--- FG_BASE/src/main/webapp/js/angular/control/fg/fgControl.js
+++ FG_BASE/src/main/webapp/js/angular/control/fg/fgControl.js
@@ -7,6 +7,12 @@ var app = angular.module('app', []);
 app.controller('MainCtrl', function($scope){
 	$scope.message = 'World';
 	
+	$scope.data = {
+		selectData: []	
+	};
+	
+	$scope.country={};
+	
 });
 
 /***
@@ -18,20 +24,35 @@ app.controller('MainCtrl', function($scope){
 app.directive('customSelectBox', function(){
 	return {
 		restrict: 'E',
-		template: '<select name="select" ng-options="d.id as d.name for d in data.selectData"></select>',
-        link:function(scope,elem,attrs){
-        	scope.data = {
-    			selectData: []	
-    		};
+		template: '<select name="select"  ng-model="selData" ng-options="d.id as d.name for d in data.selectData"></select>',
+        link:function(scope, elem, attrs){
     		var obj = {};
     		obj.id = '1';
     		obj.name = '홍길동';
-    		$scope.data['selectData'].push(obj);
+    		scope.data['selectData'].push(obj);
     		var obj = {};
     		obj.id = '2';
     		obj.name = '김길동';
-    		$scope.data['selectData'].push(obj);
-    		console.log($scope.data.selectData);        	
+    		scope.data['selectData'].push(obj);
+    		
+    		scope.selData = '2';
         }
-	}
+	};
+});
+
+app.directive('sel', function() {
+    return {
+      template: '<select ng-model="selectedValue" ng-options="c.name for c in countries"></select>',
+      restrict: 'E',
+      scope: {
+         selectedValue: '='
+      },
+      link:function(scope,elem,attrs){
+         scope.countries = [
+		      {name:'Vereinigte Arabische Emirate', value:'AE'},
+		     {name:'Andorra', value:'AD'},
+		];
+         scope.selectedValue=scope.countries[1];
+      }
+    };
 });
